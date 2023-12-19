45158be297e3a82d93e1cb20f802c3eef89cb0b5
vishishtkapoor <vishishtkapoor2@gmail.com>
2023-12-19 23:58:30 +0530

    route

--- routes/ContactRoutes.js
+++ routes/ContactRoutes.js
@@ -2,8 +2,21 @@ const express = require('express');
 const app = express.Router();
 const port = 3000;
 
-Router.route((req, res) => {
-    res.json({ message: "get all contact" });})
+Router.route("/").get((req, res) => {
+    res.status(200).json({ message: "get all contact" })
+});
+
+Router.route("/").post((req, res) => {
+    res.status(200).json({ message: "get all contact" })
+});
+
+Router.route("/").put((req, res) => {
+    res.status(200).json({ message: "update contact" })
+});
+
+Router.route("/").delete((req, res) => {
+    res.status(200).json({ message: "get all contact" })
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
